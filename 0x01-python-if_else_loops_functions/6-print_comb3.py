dc1b7452ea689e28490dfdd99535b5a8d63cd2ff
randaaz <randaaziz972@gmail.com>
2023-10-31 02:27:57 -0700

    fix error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for tens in range(10):
     for ones in range(10):
         if (tens != ones and tens < ones):
             print("{:d}{:d}".format(tens, ones),
-                  end="\n" if tens == 8 else ", ")
+                    end="\n" if tens == 8 else ", ")
