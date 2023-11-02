f2e67db170f79b0d89219afb401818ba79ca8fce
randaaz <randaaziz972@gmail.com>
2023-11-02 02:30:29 -0700

    fix error

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -5,7 +5,7 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(a, i)
-            return c
+        return c
     else:
         return sub(a, b)
     return 0
