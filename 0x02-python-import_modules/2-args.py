ab0ba4d8700ff46a341f6d4d215b23fc363391a1
randaaz <randaaziz972@gmail.com>
2023-11-02 02:15:53 -0700

    fix error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if i == 0:
         print("0 arguments.")
     elif i == 1:
-        print("1 arguments.")
+        print("1 argument.")
     else:
         print("{} arguments:".format(i))
     for j in range(i):
