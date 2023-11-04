264755bf71be7f16acf7453f7fc99ff32ffcc868
randaaz <randaaziz972@gmail.com>
2023-11-03 20:34:37 -0700

    modfy

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,13 +7,13 @@
 
 void print_python_list_info(PyObject *p)
 {
-	int  volume, assign, j;
+	int volume, assign, j;
 	PyObject *o;
 
 	volume = Py_SIZE(p);
 	assign = ((PyListObject *)p)->allocated;
 
-	printf("[*] Size of the Python list = %d\n", volume);
+	printf("[*] Size of the Python List = %d\n", volume);
 	printf("[*] Allocated = %d\n", assign);
 
 	for (j = 0; j < volume; j++)
