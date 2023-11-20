b493f59541131e799f28ae0c2e9c7599c6c953ce
randaaz <randaaziz972@gmail.com>
2023-11-20 01:38:20 -0800

    modfy

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,5 +7,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         resu = None
     finally:
-        print("Inside result {}".format(resu))
+        print("Inside result: {}".format(resu))
         return resu
