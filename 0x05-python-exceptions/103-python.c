5ac9025d5bd1807b8262abc1c752e0d0c7fb66b1
randaaz <randaaziz972@gmail.com>
2023-11-20 02:20:05 -0800

    modfy

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -7,33 +7,6 @@ void print_python_bytes(PyObject *p);
 void print_python_float(PyObject *p);
 
 /**
- * print_python_list - Prints information about a Python list
- * @p: PyObject representing a Python list
- */
-void print_python_list(PyObject *p)
-{
-	int i;
-
-	setbuf(stdout, NULL);
-	printf("[*] Python list info\n");
-	if (strcmp(p->ob_type->tp_name, "list"))
-	{
-		printf("  [ERROR] Invalid List Object\n");
-		return;
-	}
-	printf("[*] Size of the Python List = %lu\n", ((PyVarObject *)p)->ob_size);
-	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-	for (i = 0; i < ((PyVarObject *)p)->ob_size; i++)
-	{
-		printf("Element %d: %s\n", i,
-				((PyListObject *)p)->ob_item[i]->ob_type->tp_name);
-		if (!strcmp(((PyListObject *)p)->ob_item[i]->ob_type->tp_name, "bytes"))
-			print_python_bytes(((PyListObject *)p)->ob_item[i]);
-		else if (!strcmp(((PyListObject *)p)->ob_item[i]->ob_type->tp_name, "float"))
-			print_python_float(((PyListObject *)p)->ob_item[i]);
-	}
-}
-/**
  * print_python_bytes - Print information about a Python bytes object
  * @p: PyObject pointer to the bytes object
  *
@@ -79,5 +52,34 @@ void print_python_float(PyObject *p)
 	data = ((PyFloatObject *)p)->ob_fval;
 	printf("  value: %s\n",
 			PyOS_double_to_string(data, 'r', 0, Py_DTSF_ADD_DOT_0, NULL));
+}
+
+/**
+ * print_python_list - Prints information about a Python list
+ * @p: PyObject representing a Python list
+ */
+
+void print_python_list(PyObject *p)
+{
+	int i;
+
+	setbuf(stdout, NULL);
+	printf("[*] Python list info\n");
+	if (strcmp(p->ob_type->tp_name, "list"))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+	printf("[*] Size of the Python List = %lu\n", ((PyVarObject *)p)->ob_size);
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (i = 0; i < ((PyVarObject *)p)->ob_size; i++)
+	{
+		printf("Element %d: %s\n", i,
+				((PyListObject *)p)->ob_item[i]->ob_type->tp_name);
+		if (!strcmp(((PyListObject *)p)->ob_item[i]->ob_type->tp_name, "bytes"))
+			print_python_bytes(((PyListObject *)p)->ob_item[i]);
+		else if (!strcmp(((PyListObject *)p)->ob_item[i]->ob_type->tp_name, "float"))
+			print_python_float(((PyListObject *)p)->ob_item[i]);
+	}
 }
 
