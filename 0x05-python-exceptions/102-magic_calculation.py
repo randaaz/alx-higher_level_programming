6c0c9fac095710aeebaa955bbed8499aaffcbeab
randaaz <randaaziz972@gmail.com>
2023-11-20 01:30:56 -0800

    modfy

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    r = 0
-    for j in range(1, 3):
+    result = 0
+    for i in range(1, 3):
         try:
-            if j > a:
+            if i > a:
                 raise Exception('Too far')
-            r += a ** b / j
+            result += a ** b / i
         except Exception:
-            r = b + a
+            result = b + a
             break
-        return r
+        return result
