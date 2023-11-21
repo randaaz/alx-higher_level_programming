b56d6c779856cceade1a8930880dc25b3d28638f
randaaz <randaaziz972@gmail.com>
2023-11-21 01:48:50 -0800

    modfy

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -78,13 +78,14 @@ class Square:
             [print("#", end="") for c in range(0, self.__size)]
             print("")
 
-    def __str__(self):
-        """Printing same behavior as my_print()."""
-        if self.__size != 0:
-            [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for r in range(0, self.__position[0])]
-            [print("#", end="") for c in range(0, self.__size)]
+
+def __str__(self):
+    """String representation of the square."""
+    if self.__size != 0:
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             if i != self.__size - 1:
                 print("")
-        return("")
+        return ("")
