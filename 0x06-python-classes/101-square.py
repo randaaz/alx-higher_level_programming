b719e0726fc1be78d0e9de83141a442f8156331c
randaaz <randaaziz972@gmail.com>
2023-11-21 02:13:48 -0800

    modfy

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -82,8 +82,8 @@ class Square:
         if self.__size != 0:
             [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
+            [print(" ", end="") for r in range(0, self.__position[0])]
+            [print("#", end="") for c in range(0, self.__size)]
             if i != self.__size - 1:
                 print("")
         return ("")
