dd3cefce868ed8563d1111d87f401d005349e9b8
randaaz <randaaziz972@gmail.com>
2023-11-27 01:36:29 -0800

    modfy

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -72,7 +72,17 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-         """Compare two rectangles and return the bigger or equal one."""
+        """Compare two rectangles and return the bigger or equal one
+
+        Args:
+            rect_1: first  rectangles
+            rect_2: second rectangles
+
+        Raises:
+            TypeError: if rect_1 or rect_2 are not instances
+        Returns:
+            The biggest rectangle
+        """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
