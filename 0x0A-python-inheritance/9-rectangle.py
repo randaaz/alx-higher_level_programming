539164bca0a85af7dbfffea7e9c5b001b2eb45d5
randaaz <randaaziz972@gmail.com>
2023-12-04 01:14:29 -0800

    try fix error

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -19,4 +19,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """rectangle description"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return "[Rectangle]" + str(self.__width) + "/" + str(self.__height)
