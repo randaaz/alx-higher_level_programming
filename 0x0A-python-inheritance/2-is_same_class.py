98b4e44afa30cea067812ad0b5d81715865bbd72
randaaz <randaaziz972@gmail.com>
2023-12-03 21:08:45 -0800

    function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False.

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,14 +4,6 @@
 
 
 def is_same_class(obj, a_class):
-    """
-    Function to retrieve a list of attributes and methods of an object.
-
-    Args:
-        obj (object): The object to inspect.
-        a_class (type): The class to check against.
-
-    Returns:
-        bool: True if obj is an instance of a_class, False otherwise.
-    """
+    """function that returns True if the object is exactly an instance
+    of the specified class ; otherwise False"""
     return type(obj) == a_class
