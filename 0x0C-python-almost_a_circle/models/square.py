1d067b7a0058c6ff484f2ef610352afec160f04b
randaaz <randaaziz972@gmail.com>
2023-12-06 23:58:19 -0800

    the class Square that inherits from Rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,3 +11,13 @@ class Square(Rectangle):
         """informatin about souare"""
         return "[{}] ({}) {}/{} - {}".\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """"method for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
