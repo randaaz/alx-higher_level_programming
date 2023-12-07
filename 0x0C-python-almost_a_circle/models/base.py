dd5ee0433b6a3bc1384099d66412344c233b8951
randaaz <randaaziz972@gmail.com>
2023-12-07 05:28:49 -0800

    Update the class Base by adding the static method def draw(list_rectangles, list_squares): that opens a window and draws all the Rectangles and Squares

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Base class module"""
 from json import dumps, loads
+import csv
 
 
 class Base:
@@ -62,3 +63,66 @@ class Base:
             return []
         with open(json_fi, "r", encoding="utf-8") as fi:
             return [cls.create(**c) for c in cls.from_json_string(fi.read())]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """serializes and deserializes in CSV"""
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if list_objs is not None:
+            if cls is Rectangle:
+                list_objs = [[o.id, o.width, o.height, o.x, o.y]
+                             for o in list_objs]
+            else:
+                list_objs = [[o.id, o.size, o.x, o.y]
+                             for o in list_objs]
+        with open('{}.csv'.format(cls.__name__), 'w', newline='',
+                  encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerows(list_objs)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """serializes and deserializes in CSV"""
+        from models.rectangle import Rectangle
+        from models.square import Square
+        res = []
+        with open('{}.csv'.format(cls.__name__), 'r', newline='',
+                  encoding='utf-8') as file:
+            reader = csv.reader(file)
+            for w in reader:
+                w = [int(i) for i in w]
+                if cls is Rectangle:
+                    dec = {"id": w[0], "width": w[1], "height": w[2],
+                           "x": w[3], "y": w[4]}
+                else:
+                    dec = {"id": w[0], "size": w[1],
+                           "x": w[2], "y": w[3]}
+                res.append(cls.create(**dec))
+        return res
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        import turtle
+        import time
+        from random import randrange
+        turtle.Screen().colormode(255)
+        for j in list_rectangles + list_squares:
+            tut = turtle.Turtle()
+            tut.color((randrange(255), randrange(255), randrange(255)))
+            tut.pensize(1)
+            tut.penup()
+            tut.pendown()
+            tut.setpos((j.x + tut.pos()[0], j.y - tut.pos()[1]))
+            tut.pensize(10)
+            tut.forward(j.width)
+            tut.left(90)
+            tut.forward(j.height)
+            tut.left(90)
+            tut.forward(j.width)
+            tut.left(90)
+            tut.forward(j.height)
+            tut.left(90)
+            tut.end_fill()
+
+        time.sleep(5)
