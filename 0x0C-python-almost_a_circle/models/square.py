c9de00a1868f7d686f89d759f3afc9f21c17ca63
randaaz <randaaziz972@gmail.com>
2023-12-11 07:22:33 -0800

    modfy

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """souare class"""
     def __init__(self, size, x=0, y=0, id=None):
+         """informatin about souare"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
