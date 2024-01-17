0016b5565a0d7733ea71ea17e92008ffbaec022e
randaaz <randaaziz972@gmail.com>
2024-01-17 01:48:09 -0800

    lists all shows contained in the database hbtn_0d_tvshows.

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- lists all shows contained in the database hbtn_0d_tvshows.
-SELECT g.`name` AS `genre`, COUNT(*) AS `number_of_shows` FROM `tv_genres` AS g INNER JOIN `tv_show_genres` AS t ON g.`id` = t.`genre_id` GROUP BY g.`name` ORDER BY `number_of_shows` DESC;
+SELECT m.`name` AS `genre`, COUNT(*) AS `number_of_shows` FROM `tv_genres` AS m INNER JOIN `tv_show_genres` AS r ON m.`id` = r.`genre_id` GROUP BY m.`name` ORDER BY `number_of_shows` DESC;
