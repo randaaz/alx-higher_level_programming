cb4f0119e7f8d8e481c705f1eacde53c8cfdc996
randaaz <randaaziz972@gmail.com>
2024-02-12 01:31:54 -0800

    remove docemetion

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-let array = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-for (let i = 0; i < 3; i++) {
-    console.log(array[i]);
+for (const i of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(i);
 }
