847d760dad63df7b29cf22179ef84adbe21986b4
randaaz <randaaziz972@gmail.com>
2024-02-12 01:47:39 -0800

    modfy

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-function factorial(a) {
-  if (a === 1 || isNaN(a)) {
-    return 1;
-  }
-  return a * factorial(a - 1);
+function factorial (a) {
+  return a === 0 || isNaN(a) ? 1 : a * factorial(a - 1);
 }
+
+console.log(factorial(Number(process.argv[2])));
