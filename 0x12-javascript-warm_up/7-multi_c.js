255f4d36302686dc3c087f0dbcbf29580921820f
randaaz <randaaziz972@gmail.com>
2024-02-11 22:37:06 -0800

    script that prints x times “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 //script that prints x times “C is fun”
-let a = Math.floor(Number(process.argv[2]));
+let x = Math.floor(Number(process.argv[2]));
 let i;
-if (isNaN(a)) {
+if (isNaN(x)) {
     console.log("Missing number of occurrences");
 }
 else {
-    for (i = 0; i < a; i++) {
+    for (i = 0; i < x; i++) {
         console.log("C is fun");
     }
 }
