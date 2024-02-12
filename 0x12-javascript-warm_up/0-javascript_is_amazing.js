6b412f8816897737ed2da0633eb37365aecb5138
randaaz <randaaziz972@gmail.com>
2024-02-12 01:05:37 -0800

    Update this script by adding a new function incr that increments the integer value.

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // Declaring a variable named myVar and initializing it with the string value
-let myVar = "JavaScript is amazing";
+const myVar = "JavaScript is amazing";
 console.log(myVar);
