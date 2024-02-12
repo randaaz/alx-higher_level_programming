7c91f3a8931543f6955241c785e60e030d4bb75d
randaaz <randaaziz972@gmail.com>
2024-02-12 01:34:07 -0800

    remove docemetion

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const x = Math.floor(Number(process.argv[2]));
-let i;
 if (isNaN(x)) {
     console.log('Missing number of occurrences');
 }
 else {
-    for (i = 0; i < x; i++) {
+    for (let i = 0; i < x; i++) {
         console.log('C is fun');
     }
 }
