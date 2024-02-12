2dea6a0ccebb2ad24a6d8afd038749c8c3054b98
randaaz <randaaziz972@gmail.com>
2024-02-12 01:14:32 -0800

    script that prints a message depending of the number of arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const arg = process.argv.lenght;
-arg === 2 ? console.log("No argument") : arg === 3 ? console.log("Argument found") : console.log("Arguments found");
+arg === 2 ? console.log('No argument') : arg === 3 ? console.log('Argument found') : console.log('Arguments found');
