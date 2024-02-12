7e032fd03e9a01f4a8b4537406052ddb9a501404
randaaz <randaaziz972@gmail.com>
2024-02-12 01:40:03 -0800

    remove docemetion

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-let x = Math.floor(Number(process.argv[2]));
-let j;
-
+const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
     console.log('Missing size');
 }
 else {
-    for (i = 0; i < x; i++) {
+    for (let i = 0; i < x; i++) {
         let a = "";
-        for (j = 0; j < x; j++) {
+        for (let j = 0; j < x; j++) {
             a += "X";
         }
         console.log(a);
