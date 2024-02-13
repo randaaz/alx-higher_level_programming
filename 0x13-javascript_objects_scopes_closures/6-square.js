182747a8a5aae53e22865e614736b6e78568a10d
randaaz <randaaziz972@gmail.com>
2024-02-13 01:46:56 -0800

    fix the cod

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
-const SquareP = require('./5-square.js');
+const Square2 = require('./5-square');
 
-class Square extends SquareP {
-    charPrint(c) {
-      if (c === undefined) {
-        c = 'X';
-        }
-        for (let i = 0; i < this.height; i++) {
-            let x = '';
-            for (let j = 0; j < this.width; j++) {
-                x += c;
-            }
-            console.log(x);
-        }
+class Square extends Square2 {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
+    for (let i = 0; i < this.height; i++) {
+      let x = '';
+      for (let j = 0; j < this.width; j++) {
+        x += c;
+      }
+      console.log(s);
+    }
+  }
 }
 
 module.exports = Square;
