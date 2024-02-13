4ac757ded62ac31639903ecc23d81caa5c4fee16
randaaz <randaaziz972@gmail.com>
2024-02-12 23:42:22 -0800

    a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 
-const r1 = Object.entries(dict);
+const tl = Object.entries(dict);
 const v = Object.values(dict);
 const vu = [...new Set(v)];
 const nd = {};
 for (const j in vu) {
   const list = [];
-  for (const i in r1) {
-    if (r1[i][1] === vu[j]) {
-      list.unshift(r1[i][0]);
+  for (const i in tl) {
+    if (tl[i][1] === vu[j]) {
+      list.unshift(tl[i][0]);
     }
   }
   nd[vu[j]] = list;
