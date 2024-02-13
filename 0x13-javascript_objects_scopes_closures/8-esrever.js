bd1f4ec68816b8fb9f5ff2abc49f2e04d284dce2
randaaz <randaaziz972@gmail.com>
2024-02-12 23:02:11 -0800

    function that returns the reversed version of a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let l = list.length -1;
+  let i = 0;
+  while ((l - 1) > 0) {
+    let temp = list[l];
+    list[l] = list[i];
+    list[i] = temp;
+    i++;
+    l--;
+  }
+  return list;
+}
