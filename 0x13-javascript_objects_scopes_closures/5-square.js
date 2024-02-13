db6107fa0d81f2087ecdc0bf8b7a289573ec1c2c
randaaz <randaaziz972@gmail.com>
2024-02-13 01:49:45 -0800

    fix the cod

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle.js');
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-	constructor (size) {
-		super(size, size);
-	}
+  constructor (size) {
+    super(size, size);
+  }
 }
 
 module.exports = Square;
