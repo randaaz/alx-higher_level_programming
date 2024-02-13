4163543ba924cfd0280f93c4517b0725ca7e08a5
randaaz <randaaziz972@gmail.com>
2024-02-13 01:51:10 -0800

    fix the cod

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -17,17 +17,16 @@ class Rectangle {
     }
   }
 
-  rotate() {
+  rotate () {
     const temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
 
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
-
 }
 
 module.exports = Rectangle;
