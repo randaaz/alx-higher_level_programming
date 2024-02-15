07686c1330239625a7d2fa14d341c668c414fbdf
randaaz <randaaziz972@gmail.com>
2024-02-14 22:58:28 -0800

    a script that prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,10 +13,10 @@ from sqlalchemy.orm import sessionmaker
 from model_state import State
 
 if __name__ == "__main__":
-    eg = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Session = sessionmaker(bind=eg)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     for state in session.query(State).order_by(State.id):
