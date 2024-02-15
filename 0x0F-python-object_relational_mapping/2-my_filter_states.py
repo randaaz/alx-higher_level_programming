5fa93359c4c0169c50e100285c86ffcab9e212ca
randaaz <randaaziz972@gmail.com>
2024-02-14 22:07:09 -0800

    a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This script connects to a MySQL database and retrieves
+records from the 'states' table based on a provided state name pattern.
+It takes four command-line
+arguments: <username>, <password>, <database>, and <state_name_pattern>.
+The script queries the 'states' table
+for records whose 'name' column matches the provided pattern.
+It prints the details of the matching states,
+ordered by ID in ascending order.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    d = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
+                        passwd=sys.argv[2], d=sys.argv[3])
+    s = d.cursor()
+    s.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(sys.argv[4]))
+    st = s.fetchall()
+    for t in st:
+        if t[1] == sys.argv[4]:
+            print(t)
+    s.close()
+    d.close()
