6b691327fcf47ca1c6c2735c7617a172b6814bfc
randaaz <randaaziz972@gmail.com>
2024-02-15 22:55:15 -0800

    fix the code

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,13 +14,13 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
-                        passwd=sys.argv[2], db=sys.argv[3])
-    s = db.cursor()
-    s.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+                         passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
     id ASC".format(sys.argv[4]))
-    states = s.fetchall()
-    for t in states:
-        if t[1] == sys.argv[4]:
-            print(t)
-    s.close()
+    states = c.fetchall()
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
+    c.close()
     db.close()
