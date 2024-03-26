e0ea74c1a2b5446479d69813f95260cc13e45644
randaaz <randaaziz972@gmail.com>
2024-03-26 02:53:11 -0700

    modify

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,19 +7,19 @@ request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    const comp = {};
-    const tas = JSON.parse(body);
-    for (const j in tas) {
-      const task = tas[j];
-      if (task.comp === true) {
-        if (comp[task.userId] === undefined) {
-          comp[task.userId] = 1;
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
         } else {
-          comp[task.userId]++;
+          completed[task.userId]++;
         }
       }
     }
-    console.log(comp);
+    console.log(completed);
   } else {
     console.log('An error occured. Status code: ' + response.statusCode);
   }
